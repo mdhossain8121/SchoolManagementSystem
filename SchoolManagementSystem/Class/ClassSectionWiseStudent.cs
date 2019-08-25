c2b8026abae9aa661f1288ad710ed107f5edc781
unknown <you@example.com>
2019-08-25 22:40:48 +0600

    Class Section wise student interface and class

---
+++ SchoolManagementSystem/Class/ClassSectionWiseStudent.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace SchoolManagementSystem.Class
+{
+    class ClassSectionWiseStudent: Base
+    {
+        public int Id { get; set; }
+
+        public int ClassSectionId { get; set; }
+
+        public int StudentId { get; set; }
+
+        public int Roll { get; set; }
+
+        public int Year { get; set; }
+
+        public int ActiveStatus { get; set; }
+
+        private static readonly String tblClassSectionWiseStudent = "class_section_wise_student_tbl";
+
+
+        public bool Insert()
+        {
+            Command = CommandBuilder("insert into " + tblClassSectionWiseStudent + " (CLASSSECTION_ID, STUDENT_ID, ROLL, YEAR) values(@classSectionId, @studentId, @roll, @year)");
+            Command.Parameters.AddWithValue("@classSectionId", ClassSectionId);
+            Command.Parameters.AddWithValue("@studentId", StudentId);
+            Command.Parameters.AddWithValue("@roll", Roll);
+            Command.Parameters.AddWithValue("@year", Year);
+            return Execute(Command);
+        }
+
+        public bool Update()
+        {
+            Command = CommandBuilder("update " + tblClassSectionWiseStudent + " set CLASSSECTION_ID = @classSectionId, STUDENT_ID = @studentId, ROLL = @roll, YEAR = @year where ID = @id");
+            Command.Parameters.AddWithValue("@classSectionId", ClassSectionId);
+            Command.Parameters.AddWithValue("@studentId", StudentId);
+            Command.Parameters.AddWithValue("@roll", Roll);
+            Command.Parameters.AddWithValue("@year", Year);
+            return Execute(Command);
+        }
+
+        public bool Delete()
+        {
+            Command = CommandBuilder("delete from " + tblClassSectionWiseStudent + " where ID = @id");
+            Command.Parameters.AddWithValue("@id", Id);
+            return Execute(Command);
+        }
+
+        public bool SelectById()
+        {
+            Command = CommandBuilder("select ID, CLASS_NAME from " + tblClassSectionWiseStudent + " where ID = @id");
+            Command.Parameters.AddWithValue("@id", Id);
+            Reader = ExecuteReader(Command);
+            //while (Reader.Read())
+            //{
+            //    ClassName = Reader["CLASS_NAME"].ToString();
+            //    return true;
+            //}
+            return false;
+        }
+
+        public DataSet Select()
+        {
+            Command = CommandBuilder("select ID, CLASS_NAME from " + tblClassSectionWiseStudent);
+            //if (ClassName != null && !ClassName.Trim().Equals(""))
+            //{
+            //    Command.CommandText += " Where CLASS_NAME like @search";
+            //    Command.Parameters.AddWithValue("@search", "%" + ClassName + "%");
+            //}
+            return ExecuteDataSet(Command);
+        }
+    }
+}
---
+++ SchoolManagementSystem/ClassManager/ClassSectionWiseStudentManager.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using SchoolManagementSystem.Class;
+using System.Data;
+
+namespace SchoolManagementSystem.ClassManager
+{
+    class ClassSectionWiseStudentManager
+    {
+        public string SaveClass(ClassSectionWiseStudent aClassSectionWiseStudent)
+        {
+            if (aClassSectionWiseStudent.Insert())
+            {
+                return "Data has been successfully saved";
+            }
+            else
+            {
+                File.AppendAllText("logs.txt", DateTime.Now.ToString() + " :: Assigning student to class (Insert)" + Environment.NewLine);
+                File.AppendAllText("logs.txt", DateTime.Now.ToString() + " :: " + aClassSectionWiseStudent.Error + Environment.NewLine);
+                return aClassSectionWiseStudent.Error;
+            }
+        }
+
+        public String UpdateClass(ClassSectionWiseStudent aClassSectionWiseStudent)
+        {
+            if (aClassSectionWiseStudent.Update())
+            {
+                return "Data has been successfully updated";
+            }
+            else
+            {
+                File.AppendAllText("logs.txt", DateTime.Now.ToString() + " :: Assigning student to class (Update)" + Environment.NewLine);
+                File.AppendAllText("logs.txt", DateTime.Now.ToString() + " :: " + aClassSectionWiseStudent.Error + Environment.NewLine);
+                return aClassSectionWiseStudent.Error;
+            }
+        }
+
+        public string DeleteClass(ClassSectionWiseStudent aClassSectionWiseStudent)
+        {
+            if (aClassSectionWiseStudent.Delete())
+            {
+                return "Data has been successfully deleted";
+            }
+            else
+            {
+                File.AppendAllText("logs.txt", DateTime.Now.ToString() + " :: Assigning student to class (Delete) " + Environment.NewLine);
+                File.AppendAllText("logs.txt", DateTime.Now.ToString() + " :: " + aClassSectionWiseStudent.Error + Environment.NewLine);
+                return aClassSectionWiseStudent.Error;
+            }
+        }
+
+        public DataTable GetAllClassData(ClassSectionWiseStudent aClassSectionWiseStudent)
+        {
+            DataSet ds = aClassSectionWiseStudent.Select();
+            if (ds == null)
+                return null;
+            return ds.Tables[0];
+        }
+    }
+}
