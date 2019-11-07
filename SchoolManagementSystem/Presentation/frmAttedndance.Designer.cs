a7c8397e53cf1ab03efe67730beafe07acb4e2f9
deadhossain <md.hossain8121@gmail.com>
2019-11-07 09:32:54 +0600

    attendance form automate design

--- SchoolManagementSystem/Presentation/frmAttedndance.Designer.cs
+++ SchoolManagementSystem/Presentation/frmAttedndance.Designer.cs
@@ -28,58 +28,121 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.colStudentName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.colStatus = new System.Windows.Forms.DataGridViewComboBoxColumn();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.progressBar_V1 = new System.Windows.Forms.ProgressBar();
+            this.pictureBoxV1 = new System.Windows.Forms.PictureBox();
+            this.lblDeviceStatus = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.StatusBar = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxV1)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // groupBox1
             // 
-            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
-            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.colStudentName,
-            this.colStatus});
-            this.dataGridView1.GridColor = System.Drawing.SystemColors.ControlLight;
-            this.dataGridView1.Location = new System.Drawing.Point(29, 84);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(856, 332);
-            this.dataGridView1.TabIndex = 0;
+            this.groupBox1.Controls.Add(this.lblDeviceStatus);
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(582, 56);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Device";
             // 
-            // colStudentName
+            // btnConnect
             // 
-            this.colStudentName.HeaderText = "Student Name";
-            this.colStudentName.Name = "colStudentName";
+            this.btnConnect.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(32, 16);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(143, 31);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
             // 
-            // colStatus
+            // progressBar_V1
             // 
-            this.colStatus.HeaderText = "Status";
-            this.colStatus.Items.AddRange(new object[] {
-            "Present",
-            "Absent"});
-            this.colStatus.Name = "colStatus";
-            this.colStatus.Resizable = System.Windows.Forms.DataGridViewTriState.True;
-            this.colStatus.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.progressBar_V1.Location = new System.Drawing.Point(78, 230);
+            this.progressBar_V1.Name = "progressBar_V1";
+            this.progressBar_V1.Size = new System.Drawing.Size(143, 12);
+            this.progressBar_V1.TabIndex = 60;
+            // 
+            // pictureBoxV1
+            // 
+            this.pictureBoxV1.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBoxV1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBoxV1.Location = new System.Drawing.Point(78, 80);
+            this.pictureBoxV1.Name = "pictureBoxV1";
+            this.pictureBoxV1.Size = new System.Drawing.Size(143, 146);
+            this.pictureBoxV1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxV1.TabIndex = 59;
+            this.pictureBoxV1.TabStop = false;
+            // 
+            // lblDeviceStatus
+            // 
+            this.lblDeviceStatus.AutoSize = true;
+            this.lblDeviceStatus.Font = new System.Drawing.Font("Papyrus", 12F, System.Drawing.FontStyle.Bold);
+            this.lblDeviceStatus.Location = new System.Drawing.Point(191, 19);
+            this.lblDeviceStatus.Name = "lblDeviceStatus";
+            this.lblDeviceStatus.Size = new System.Drawing.Size(373, 25);
+            this.lblDeviceStatus.TabIndex = 1;
+            this.lblDeviceStatus.Text = "Please Connect the device and click on connect";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Font = new System.Drawing.Font("Algerian", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(312, 123);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(225, 54);
+            this.lblTime.TabIndex = 61;
+            this.lblTime.Text = "00:00:00";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // StatusBar
+            // 
+            this.StatusBar.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.StatusBar.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.StatusBar.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.StatusBar.Location = new System.Drawing.Point(0, 264);
+            this.StatusBar.Name = "StatusBar";
+            this.StatusBar.Size = new System.Drawing.Size(588, 24);
+            this.StatusBar.TabIndex = 62;
             // 
             // frmAttedndance
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(921, 444);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(588, 288);
+            this.Controls.Add(this.StatusBar);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.progressBar_V1);
+            this.Controls.Add(this.pictureBoxV1);
+            this.Controls.Add(this.groupBox1);
             this.Name = "frmAttedndance";
             this.Text = "frmAttedndance";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxV1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn colStudentName;
-        private System.Windows.Forms.DataGridViewComboBoxColumn colStatus;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ProgressBar progressBar_V1;
+        private System.Windows.Forms.PictureBox pictureBoxV1;
+        private System.Windows.Forms.Label lblDeviceStatus;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label StatusBar;
     }
 }
